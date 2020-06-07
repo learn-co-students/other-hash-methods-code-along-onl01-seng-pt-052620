c9770e9af8b2343bb2730533a22488b18f1f4805
edmundo papi <edmundopierre@yahoo.com>
2020-06-07 10:52:13 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -10,4 +10,11 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
+ empty_array = []
+
+ groceries.each do |key, value|
+   empty_array << value
+ end
+ empty_array.flatten
+
 end
